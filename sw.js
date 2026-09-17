c8987be1b5be44252bd4913b5cbe6321c96dce72
IKGlitch <igor.kurczewski@autobahn.tech>
2026-09-17 10:24:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* John Money offline cache. Bump CACHE when index.html changes. */
-const CACHE = 'johnmoney-v3';
-const ASSETS = ['./','./index.html','./manifest.json','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+const CACHE = 'johnmoney-v11';
+const ASSETS = ['./','./index.html','./manifest.json','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
